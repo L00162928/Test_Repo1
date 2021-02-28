6209a81d9ae9ef8ed009972d72a1f36b1581524c
Alwyn Benny <L00162928@student.lyit.ie>
2021-02-28 18:31:12 +0000

    Initial files added

---
+++ src/main/java/mavengithubactionsdemo/MvngitApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mavengithubactionsdemo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class MvngitApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello from Alwyn");
+
+	}
+
+}
